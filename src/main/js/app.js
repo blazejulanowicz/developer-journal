5d34daab9d2a65b59cc6d0ea25cf62536aefe857
sirgentleman <imnotfreeman@gmail.com>
2021-02-04 20:01:41 +0100

    fix not working react

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const axios = require('axios');
+const regeneratorRuntime = require("regenerator-runtime");
 
 class App extends React.Component {
 
@@ -12,5 +13,17 @@ class App extends React.Component {
     async componentDidMount() {
         const response = await axios.get("http://localhost:8080/api/users");
         console.log(response.data);
+        this.setState({users: response.data._embedded.users});
+    }
+
+    render() {
+        return (
+            <div>SIEMANO KOLANO</div>
+        )
     }
 }
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('react')
+)
