4cda6d4709eb26f230589798f314aa39ba6a6c1a
sirgentleman <imnotfreeman@gmail.com>
2021-02-27 15:35:41 +0100

    fix filter without any project bug

--- src/main/js/api/uriTemplateListInterceptor.js
+++ src/main/js/api/uriTemplateListInterceptor.js
@@ -9,13 +9,15 @@ define(function(require) {
                 return request;
             Object.keys(request.params).forEach((key) => {
                 if(request.params[key] instanceof Array) {
-                    let j = 0;
-                    if(request.path.indexOf('?') === -1) {
-                        request.path = request.path.concat('?'+key+'='+request.params[key][0]);
-                        j = 1;
+                    if(request.params[key].length !== 0) {
+                        let j = 0;
+                        if(request.path.indexOf('?') === -1) {
+                            request.path = request.path.concat('?'+key+'='+request.params[key][0]);
+                            j = 1;
+                        }
+                        for(j; j < request.params[key].length; j++)
+                            request.path = request.path.concat('&'+key+'='+request.params[key][j]);
                     }
-                    for(j; j < request.params[key].length; j++)
-                        request.path = request.path.concat('&'+key+'='+request.params[key][j]);
                     delete request.params[key];
                 }
             });
