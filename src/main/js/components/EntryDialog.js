83c2b3af19a5e005236d1f5434b27c0c8b00e066
sirgentleman <imnotfreeman@gmail.com>
2021-03-16 13:47:56 +0100

    add cancel button and hide entry dialog

--- src/main/js/components/EntryDialog.js
+++ src/main/js/components/EntryDialog.js
@@ -10,11 +10,13 @@ class EntryDialog extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleExpand = this.handleExpand.bind(this);
         this.handleProjectChange = this.handleProjectChange.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
         this.projectRef = React.createRef();
         this.contentRef = React.createRef();
         this.iconRef = React.createRef();
 
         this.state = {
+            isExpanded: false,
             commitSHAInput: false,
             selectedProject: {},
         }
@@ -48,9 +50,11 @@ class EntryDialog extends React.Component {
     }
 
     handleExpand(event) {
+        event.preventDefault();
         event.currentTarget.childNodes[0].classList.toggle('rotate');
         let mainDiv = event.currentTarget.parentNode;
         mainDiv.classList.toggle('new-entry-nexpanded');
+        this.setState({...this.state, isExpanded: !this.state.isExpanded})
     }
 
     handleProjectChange(event) {
@@ -62,9 +66,18 @@ class EntryDialog extends React.Component {
             this.setState({commitSHAInput: false});
     }
 
+    handleBlur(event) {
+        event.preventDefault();
+        let currentTarget= event.currentTarget;
+        setTimeout(() => {
+            if (!currentTarget.contains(document.activeElement) && this.state.isExpanded) {
+                this.iconRef.current.click();
+            }},0);
+    }
+
     render() {
         return (
-            <div id="createEntry" className="entry new-entry-nexpanded">
+            <div onBlur={this.handleBlur} id="createEntry" className="entry new-entry-nexpanded">
                     <div ref={this.iconRef} onClick={this.handleExpand}><FontAwesomeIcon className='expand-icon' icon={faPlus}/></div>
                     <div className='animated'>
                         <h1>Create new entry</h1>
@@ -76,6 +89,10 @@ class EntryDialog extends React.Component {
                                     {this.props.projects.map(project => <option key={project.entity._links.self.href} value={project.entity._links.self.href}>{project.entity.name}</option>)}
                                 </select>
                             </div>
+                            <button className='button submit-button' onClick={(event) => {
+                                event.preventDefault();
+                                this.iconRef.current.click();
+                            }}>CANCEL</button>
                             <button className='button submit-button' onClick={this.handleSubmit}>SUBMIT</button>
                         </form>
                     </div>
